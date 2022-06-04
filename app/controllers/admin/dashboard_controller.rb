52e6038da8f3b824ee65c9c3202888c1b8acbcb1
George-was-here <george.burt@gmail.com>
2022-06-04 06:33:00 +0000

    Adding dashboard with product and order counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,9 @@
 class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with :name => ENV['ADMIN_NAME'], :password => ENV['ADMIN_PASSWORD']
+
   def show
+    @products_length = Product.order(id: :desc).all.length
+    @orders_length = Order.order(id: :desc).all.length
   end
+
 end
