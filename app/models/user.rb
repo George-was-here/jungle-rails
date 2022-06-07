d6e9f7fbdf05a4edd346a6bedfb738060d24db4b
George-was-here <george.burt@gmail.com>
2022-06-07 05:55:57 +0000

    Adding user field rSpec testing and user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ActiveRecord::Base
-
   has_secure_password
+  validates :email, uniqueness: true
+  validates :email, presence: true
+  validates :name, presence: true
 
+  validates :password, length: { minimum: 5, maximum: 20 }, on: :create
+  validates :password, presence: true, if: :password_confirmation_given?
+    def password_confirmation_given?
+      password_confirmation.nil? ? false : !password_confirmation.empty?
+    end
+    
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+
+    it "should create a user when all four fields set" do
+      @user = User.create({name: "Zoe", email: "zoe@dogue.com", password: 'bones', password_confirmation: 'bones'})
+      expect(@user).to be_valid
+    end
+
+    it "should fail create a user when password field does not match" do
+      @user = User.create({name: "Zoe", email: "zoe@dogue.com", password: "bones", password_confirmation: "b0nes"})
+      expect(@user).to_not be_valid
+    end
+
+    it "should fail create a user when there is no name given" do
+      @user = User.create({name: nil, email: "zoe@dogue.com", password: "bones", password_confirmation: "bones"})
+      expect(@user).to_not be_valid
+    end
+
+    it "should fail create a user when there is no email given" do
+      @user = User.create({name: "Zoe", email: nil, password: "bones", password_confirmation: "bones"})
+      expect(@user).to_not be_valid
+    end
+
+    it "should fail create a user when there is no password given" do
+      @user = User.create({name: "Zoe", email: "zoe@dogue.com", password: nil, password_confirmation: nil})
+      expect(@user).to_not be_valid
+    end
+
+    it "should fail to create a user when a user already exists with that email" do
+      @user = User.create({name: "Zoe", email: "zoe@dogue.com", password: 'bones', 
+      password_confirmation: 'bones'})
+      @user2 = User.create({name: "Zoe", email: "zoe@dogue.com", password: 'bones', 
+      password_confirmation: 'bones'})
+      expect(@user2).to_not be_valid
+    end
+
+    it "should fail create a user when there is a password shorter than 5 character" do
+      @user = User.create({name: "Zoe", email: "zoe@dogue.com", password: "bop", password_confirmation: "bop"})
+      expect(@user).to_not be_valid
+    end
+  end  
 end
