f0502033a22ca6cffa2fd314d253f19ad2d125d2
George-was-here <george.burt@gmail.com>
2022-06-07 06:15:12 +0000

    Splitting name into first and last with tests passing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,40 +4,45 @@ RSpec.describe User, type: :model do
   describe 'Validations' do
 
     it "should create a user when all four fields set" do
-      @user = User.create({name: "Zoe", email: "zoe@dogue.com", password: 'bones', password_confirmation: 'bones'})
+      @user = User.create({first_name: "Zoe", last_name: "B", email: "zoe@dogue.com", password: 'bones', password_confirmation: 'bones'})
       expect(@user).to be_valid
     end
 
     it "should fail create a user when password field does not match" do
-      @user = User.create({name: "Zoe", email: "zoe@dogue.com", password: "bones", password_confirmation: "b0nes"})
+      @user = User.create({first_name: "Zoe", last_name: "B", email: "zoe@dogue.com", password: "bones", password_confirmation: "b0nes"})
       expect(@user).to_not be_valid
     end
 
-    it "should fail create a user when there is no name given" do
-      @user = User.create({name: nil, email: "zoe@dogue.com", password: "bones", password_confirmation: "bones"})
+    it "should fail create a user when there is no first_name given" do
+      @user = User.create({first_name: nil, last_name: "B", email: "zoe@dogue.com", password: "bones", password_confirmation: "bones"})
+      expect(@user).to_not be_valid
+    end
+
+    it "should fail create a user when there is no first_name given" do
+      @user = User.create({first_name: "Zoe", last_name: nil, email: "zoe@dogue.com", password: "bones", password_confirmation: "bones"})
       expect(@user).to_not be_valid
     end
 
     it "should fail create a user when there is no email given" do
-      @user = User.create({name: "Zoe", email: nil, password: "bones", password_confirmation: "bones"})
+      @user = User.create({first_name: "Zoe", last_name: "B", email: nil, password: "bones", password_confirmation: "bones"})
       expect(@user).to_not be_valid
     end
 
     it "should fail create a user when there is no password given" do
-      @user = User.create({name: "Zoe", email: "zoe@dogue.com", password: nil, password_confirmation: nil})
+      @user = User.create({first_name: "Zoe", last_name: "B", email: "zoe@dogue.com", password: nil, password_confirmation: nil})
       expect(@user).to_not be_valid
     end
 
     it "should fail to create a user when a user already exists with that email" do
-      @user = User.create({name: "Zoe", email: "zoe@dogue.com", password: 'bones', 
+      @user = User.create({first_name: "Zoe", last_name: "B", email: "zoe@dogue.com", password: 'bones', 
       password_confirmation: 'bones'})
-      @user2 = User.create({name: "Zoe", email: "zoe@dogue.com", password: 'bones', 
+      @user2 = User.create({first_name: "Zoe", last_name: "B", email: "zoe@dogue.com", password: 'bones', 
       password_confirmation: 'bones'})
       expect(@user2).to_not be_valid
     end
 
     it "should fail create a user when there is a password shorter than 5 character" do
-      @user = User.create({name: "Zoe", email: "zoe@dogue.com", password: "bop", password_confirmation: "bop"})
+      @user = User.create({first_name: "Zoe", last_name: "B", email: "zoe@dogue.com", password: "bop", password_confirmation: "bop"})
       expect(@user).to_not be_valid
     end
   end  
