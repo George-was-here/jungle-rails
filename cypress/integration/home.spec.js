f94c22000817d589ce2bb49ed34cf511b187ce5f
George-was-here <george.burt@gmail.com>
2022-06-08 08:08:55 +0000

    Adding second cypress test to find products

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -2,4 +2,8 @@ describe('Home', () => {
   it('Visits home page, running server', () => {
     cy.visit('/');
   });
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
 });
